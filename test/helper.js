666ca8eaf517e1a52c2c043478a26a36dc966886
Ben Murphy <benmurphy.me@gmail.com>
2018-03-01 17:34:21 +0000

    Update test helper

--- test/helper.js
+++ test/helper.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {mount} from 'enzyme';
+import {shallow} from 'enzyme';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducers from '../src/reducers'
 
 export const renderWithProvider = (ComponentClass, props = {}, state) => {
-  const instance = mount(
-    <Provider store={createStore(reducers, state)}>
+  const store = createStore(reducers, state);
+  const instance = shallow(
+    <Provider store={store}>
       <ComponentClass {...props} />
     </Provider>
   );
-  return instance.find(ComponentClass);
+  return instance.dive({context: {store}});
 };
